0d6d31f09f0ba91cd816b8ecf43c5f0f704105ed
Mostafa2291 <mostafazayed2007@gmail.com>
2026-04-12 02:06:56 +0200

    Schemer stealEnergy helper method

--- src/game/engine/monsters/Schemer.java
+++ src/game/engine/monsters/Schemer.java
@@ -1,4 +1,5 @@
 package game.engine.monsters;
+import game.engine.Constants;
 import game.engine.Role;
 
 public class Schemer extends Monster {
@@ -8,5 +9,11 @@ public class Schemer extends Monster {
     }
 
     
+    private int stealEnergyFrom(Monster target){ //disregards target shield
+        int stolen = Math.min(target.getEnergy(), Constants.SCHEMER_STEAL); //steal whatevers lower the target or schemer steal
+        
+        target.setEnergy(target.getEnergy() - stolen);
+        return stolen;
 
+    }
 }
