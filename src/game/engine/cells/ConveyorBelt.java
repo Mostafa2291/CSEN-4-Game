1182b8ace87fa528a5ea447d2c5095e6783a150d
Omar <omar.anwar737@gmail.com>
2026-04-14 17:39:48 +0200

    Update ConveyorBelt.java

--- src/game/engine/cells/ConveyorBelt.java
+++ src/game/engine/cells/ConveyorBelt.java
@@ -1,11 +1,22 @@
 package game.engine.cells;
 
+import game.engine.monsters.Monster;
+
 public class ConveyorBelt extends TransportCell {
 
     public ConveyorBelt(String name, int effect) {
         super(name, effect); 
     }
     
-
+    @Override
+    //transports the monster by changing its position according to the effect of the conveyor belt
+    public void transport(Monster monster){
+            monster.setPosition(this.getEffect()+monster.getPosition());
+    }
+    @Override
+    //when a monster lands on the conveyor belt, it gets transported
+    public void onLand(Monster landingMonster, Monster opponentMonster){
+        this.transport(landingMonster);
+    }
 
 }
