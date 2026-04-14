54deb75bf2339917c8da4d9d26f32bd91be9e3d3
Omar <omar.anwar737@gmail.com>
2026-04-14 18:37:59 +0200

    onland of monstercell

--- src/game/engine/cells/MonsterCell.java
+++ src/game/engine/cells/MonsterCell.java
@@ -17,15 +17,32 @@ public class MonsterCell extends Cell {
     public Monster getCellMonster() {
         return cellMonster;
     }
+    @Override
     public void onLand(Monster landingMonster, Monster opponentMonster){
+        //if the landing monster has the same role as the cell monster, it executes its powerup effect on the opponent monster
       if(landingMonster.getRole()==cellMonster.getRole()){
-        landingMonster.executePowerupEffect
-        
-       
+        landingMonster.executePowerupEffect(opponentMonster);
+      }
+      //if the landing monster has a different role than the cell monster
+       else{
+        //
+        if(landingMonster.getEnergy()>cellMonster.getEnergy()){
+            //if the landing monster is shielded, it loses its shield and the cell monster's energy becomes the landing monster's energy
+            if(landingMonster.isShielded()==true){
+                landingMonster.setShielded(false);
+                cellMonster.setEnergy(landingMonster.getEnergy());
+            }
+            //if the landing monster is not shielded,they swap their energy values
+                int tmpenergy=landingMonster.getEnergy();
+                landingMonster.setEnergy(cellMonster.getEnergy());
+                cellMonster.setEnergy(tmpenergy);
+            }
+           
+        }
     }
- 
+}
      
 
     
 
-}
+
