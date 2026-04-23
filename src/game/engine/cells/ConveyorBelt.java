eec9fdd91ab1caa4b616fbb01738abd2adb1945a
Mostafa2291 <mostafazayed2007@gmail.com>
2026-04-23 22:33:12 +0200

    grrr

--- src/game/engine/cells/ConveyorBelt.java
+++ src/game/engine/cells/ConveyorBelt.java
@@ -8,11 +8,7 @@ public class ConveyorBelt extends TransportCell {
         super(name, effect); 
     }
     
-    @Override
-    //transports the monster by changing its position according to the effect of the conveyor belt
-    public void transport(Monster monster){
-            monster.move(Math.abs(this.getEffect()));
-    }
+    
     @Override
     //when a monster lands on the conveyor belt, it gets transported
     public void onLand(Monster landingMonster, Monster opponentMonster){
