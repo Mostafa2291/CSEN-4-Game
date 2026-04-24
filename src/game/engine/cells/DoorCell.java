3b560852a21b84f674ee137ef4975cdbc817f789
Mostafa2291 <mostafazayed2007@gmail.com>
2026-04-24 04:15:49 +0300

    improved doorcell + fixes

--- src/game/engine/cells/DoorCell.java
+++ src/game/engine/cells/DoorCell.java
@@ -1,28 +1,20 @@
 package game.engine.cells;
-import java.util.ArrayList;
 import game.engine.Board;
 import game.engine.Role;
 import game.engine.interfaces.CanisterModifier;
 import game.engine.monsters.Monster;
 
-
 public class DoorCell extends Cell implements CanisterModifier {
-
-    //Read only attributes
-    private Role  role;
-    private int  energy;
-
-    //R&W attributes
+    private Role role;
+    private int energy;
     private boolean activated;
-
 
     public DoorCell(String name, Role role, int energy) {
         super(name);
         this.role = role;
         this.energy = energy;
-        this.activated=false;
+        this.activated = false;
     }
-
 
     public Role getRole() {
         return role;
@@ -36,53 +28,34 @@ public class DoorCell extends Cell implements CanisterModifier {
     public void setActivated(boolean isActivated) {
         this.activated = isActivated;
     }
-     @Override
-    public void modifyCanisterEnergy(Monster monster, int canisterValue){
-        monster.alterEnergy(canisterValue);
+
+    @Override
+    public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+        int magnitude = Math.abs(canisterValue);
+        if (monster.getRole() == this.role) {
+            monster.alterEnergy(magnitude);
+        } else if (monster.isShielded()) {
+            monster.setShielded(false);
+        } else {
+            monster.alterEnergy(-magnitude);
+        }
     }
 
- 
-    
-   @Override
-    public void onLand(Monster landingMonster, Monster opponentMonster){
+    @Override
+    public void onLand(Monster landingMonster, Monster opponentMonster) {
         super.onLand(landingMonster, opponentMonster);
-        
-        Role doorRole = this.getRole();
+        if (this.isActivated()) return;
         Role landingRole = landingMonster.getRole();
-        int doorEnergy = this.energy;
-
-    if(!this.isActivated()){
-        ArrayList<Monster> stationedMonsters = Board.getStationedMonsters();
-        //if the landing monster is of the same role as the door, it gets a boost in energy
-        if(landingRole==doorRole){
-            this.setActivated(true);
-            this.modifyCanisterEnergy(landingMonster,doorEnergy);
-            //monsters of the same role as the door get a boost as well in energy
-            for(int i=0;i<stationedMonsters.size();i++){
-                Role stationedRole = stationedMonsters.get(i).getRole();
-                if(stationedRole==landingRole){
-                    this.modifyCanisterEnergy(stationedMonsters.get(i),this.energy);
-                }
+        if (landingRole != this.role && landingMonster.isShielded()) {
+            landingMonster.setShielded(false);
+            return;
+        }
+        this.modifyCanisterEnergy(landingMonster, this.energy);
+        for (Monster m : Board.getStationedMonsters()) {
+            if (m.getRole() == landingRole) {
+                this.modifyCanisterEnergy(m, this.energy);
             }
         }
-        else{//else the landing monster is of a different role than the door  
-                //if the landing monster is not shielded, it gets a decrease in energy and the door gets activated
-                if(!landingMonster.isShielded()){
-                    this.setActivated(true);
-                    this.modifyCanisterEnergy(landingMonster,-this.energy);
-                    //monsters of a different role than the door get a decrease in energy as well
-                    for(int i=0;i<stationedMonsters.size();i++){
-                        Role stationedRole = stationedMonsters.get(i).getRole();
-                        if(stationedRole==landingRole){
-                            this.modifyCanisterEnergy(stationedMonsters.get(i),-this.energy);
-                    }
-                }
-                }
-                //if the landing monster is shielded, it loses its shield but does not get a decrease in energy and the door does not get activated
-                else{
-                    landingMonster.setShielded(false);
-            }
-        }
-    }
+        this.setActivated(true);
     }
 }
